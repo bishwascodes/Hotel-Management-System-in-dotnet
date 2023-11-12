cf7a46c4541988db9d634d68ec41133a360269e3
Bishwas <bishwas360@gmail.com>
2023-11-11 23:49:57 -0700

    After adding all the code to their respective classes

--- Hotel.Data/DataClass.cs
+++ Hotel.Data/DataClass.cs
@@ -3,4 +3,133 @@
 public class DataClass
 {
 
+    public static List<(int roomNumber, string type)> ReadRoomFile(string filePath)
+    {
+        List<(int roomNumber, string type)> roomList = new List<(int roomNumber, string type)>();
+
+        if (File.Exists(filePath))
+        {
+            string[] lines = File.ReadAllLines(filePath);
+            foreach (string line in lines)
+            {
+                string[] parts = line.Split(',');
+                if (parts.Length == 2 && int.TryParse(parts[0], out int roomNumber) && Enum.TryParse(parts[1], out string roomType))
+                {
+                    roomList.Add((roomNumber, roomType));
+                }
+            }
+        }
+
+        return roomList;
+    }
+
+    public static List<(Guid reservationNumber, DateTime date, int roomNumber, string customerName, string paymentConfirmation)> ReadReservationsFile(string filePath)
+    {
+        List<(Guid reservationNumber, DateTime date, int roomNumber, string customerName, string paymentConfirmation)> reservationList = new List<(Guid reservationNumber, DateTime date, int roomNumber, string customerName, string paymentConfirmation)>();
+
+        if (File.Exists(filePath))
+        {
+            string[] lines = File.ReadAllLines(filePath);
+            foreach (string line in lines)
+            {
+                string[] parts = line.Split(',');
+                if (parts.Length == 5 && Guid.TryParse(parts[0], out Guid reservationNumber) && DateTime.TryParse(parts[1], out DateTime date) &&
+                    int.TryParse(parts[2], out int roomNumber) && !string.IsNullOrEmpty(parts[3]) && !string.IsNullOrEmpty(parts[4]))
+                {
+                    reservationList.Add((reservationNumber, date, roomNumber, parts[3], parts[4]));
+                }
+            }
+        }
+
+        return reservationList;
+    }
+
+    public static List<(string customerName, long cardNumber)> ReadCustomersFile(string filePath)
+    {
+        List<(string customerName, long cardNumber)> customersList = new List<(string customerName, long cardNumber)>();
+
+        if (File.Exists(filePath))
+        {
+            string[] lines = File.ReadAllLines(filePath);
+            foreach (string line in lines)
+            {
+                string[] parts = line.Split(',');
+                if (parts.Length == 2 && !string.IsNullOrEmpty(parts[0]) && long.TryParse(parts[1], out long cardNumber))
+                {
+                    customersList.Add((parts[0], cardNumber));
+                }
+            }
+        }
+
+        return customersList;
+    }
+
+    public static List<(string roomType, decimal dailyRate)> ReadRoomPricesFile(string filePath)
+    {
+        List<(string roomType, decimal dailyRate)> roomPrices = new List<(string roomType, decimal dailyRate)>();
+
+        if (File.Exists(filePath))
+        {
+            string[] lines = File.ReadAllLines(filePath);
+            foreach (string line in lines)
+            {
+                string[] parts = line.Split(',');
+                if (parts.Length == 2 && Enum.TryParse(parts[0], out string roomType) && decimal.TryParse(parts[1], out decimal dailyRate))
+                {
+                    roomPrices.Add((roomType, dailyRate));
+                }
+            }
+        }
+
+        return roomPrices;
+    }
+
+    public static void UpdateRoomFile(string filePath, List<(int roomNumber, string type)> roomList)
+    {
+        List<string> lines = new List<string>();
+
+        foreach (var room in roomList)
+        {
+            string line = $"{room.roomNumber},{room.type}";
+            lines.Add(line);
+        }
+        File.WriteAllLines(filePath, lines);
+    }
+
+    public static void UpdateReservationsFile(string filePath, List<(Guid reservationNumber, DateTime date, int roomNumber, string customerName, string paymentConfirmation)> reservationList)
+    {
+        List<string> lines = new List<string>();
+        foreach (var reservation in reservationList)
+        {
+            string line = $"{reservation.reservationNumber},{reservation.date},{reservation.roomNumber},{reservation.customerName},{reservation.paymentConfirmation}";
+            lines.Add(line);
+        }
+        File.WriteAllLines(filePath, lines);
+    }
+
+    public static void UpdateCustomersFile(string filePath, List<(string customerName, long cardNumber)> customersList)
+    {
+        List<string> lines = new List<string>();
+        foreach (var customer in customersList)
+        {
+            string line = $"{customer.customerName},{customer.cardNumber}";
+            lines.Add(line);
+        }
+        File.WriteAllLines(filePath, lines);
+    }
+
+    public static void UpdateRoomPricesFile(string filePath, List<(string roomType, decimal dailyRate)> roomPrices)
+    {
+        List<string> lines = new List<string>();
+        foreach (var price in roomPrices)
+        {
+            string line = $"{price.roomType},{price.dailyRate}";
+            lines.Add(line);
+        }
+        File.WriteAllLines(filePath, lines);
+    }
+
+
+
+
 }
--- Hotel.Logic/LogicClass.cs
+++ Hotel.Logic/LogicClass.cs
@@ -1,10 +1,57 @@
 ﻿namespace Hotel.Logic;
+using Hotel.Data;
 
 public class LogicClass
 {
     // File Paths
-    string roomsFile = "..\\Rooms.txt";
-    string reservationsFile = "..\\Reservations.txt";
-    string customersFile = "..\\Customers.txt";
-    string roomPricesFile = "..\\RoomPrices.txt";
+    public static string roomsFile = "..\\Rooms.txt";
+    public static string reservationsFile = "..\\Reservations.txt";
+    public static string customersFile = "..\\Customers.txt";
+    public static string roomPricesFile = "..\\RoomPrices.txt";
+
+    public static List<(int roomNumber, string type)> roomList = DataClass.ReadRoomFile(roomsFile);
+    public static List<(Guid reservationNumber, DateTime date, int roomNumber, string customerName, string paymentConfirmation)> reservationList = DataClass.ReadReservationsFile(reservationsFile);
+    public static List<(string customerName, long cardNumber)> customersList = DataClass.ReadCustomersFile(customersFile);
+    public static List<(string roomType, decimal dailyRate)> roomPrices = DataClass.ReadRoomPricesFile(roomPricesFile);
+
+
+
+    // Adding items to each list 
+    public static void addToRoom((int, string) roomData)
+    {
+        roomList.Add((roomData.Item1, roomData.Item2));
+    }
+    public static void addToReservationList((Guid, DateTime, int, string, string) reservationData)
+    {
+        reservationList.Add((reservationData.Item1, reservationData.Item2, reservationData.Item3, reservationData.Item4, reservationData.Item5));
+    }
+    public static void addToCustomers((string, int) customerData)
+    {
+        customersList.Add((customerData.Item1, customerData.Item2));
+    }
+    public static void addToRoomPrice((string roomType, decimal dailyRate) roomPriceDatas)
+    {
+        roomPrices.Add((roomPriceDatas.roomType, roomPriceDatas.dailyRate));
+    }
+
+
+
+
+
+
+    // Methods 
+    public static string GenerateRandomString(int length)
+    {
+        const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        Random random = new Random();
+        char[] result = new char[length];
+
+        for (int i = 0; i < length; i++)
+        {
+            result[i] = chars[random.Next(chars.Length)];
+        }
+
+        return new string(result);
+    }
+
 }
